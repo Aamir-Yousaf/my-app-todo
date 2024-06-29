f4dc6d0baf093391f6a9b564e801f4fd6c83335e
Aamir-Yousaf <aamir.yousaf.dev@gmail.com>
2024-06-29 08:30:03 +0500

    add mark as read function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
+  const [todo, setTodo]= useState("");
+  const [error, setError]= useState(false);
+  
+  const handleSubmit = () => {
+    if(todo){
+     let newTodoLists = [...todoList];
+     newTodoLists.push({ text: todo, markAsRead: false });
+     setTodoList(newTodoLists);
+     setTodo("");
+    }
+    else{
+     setError(true); 
+    }
+  }
+  const handleMarkAsRead = () =>{
+     console.log("marked")
+  }
+  const handleChange = (e) => {
+     setTodo(e.target.value);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="text"
+        placeholder="enter your todo"
+        className="todo-input"
+        onChange={handleChange}
+        value={todo}
+      />
+      <button onClick={handleSubmit}>Submit Todo</button>
+      {error ? <p>input is empty</p> : null}
+      {todoList.map((items, i) => (
+        <div key={i}>
+          <p>{items}</p>
+          <button onClick={() => handleMarkAsRead(i)}> MarkAsRead </button>
+        </div>
+      ))}
     </div>
   );
 }
