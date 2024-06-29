a4ed601374c0842274167cae227c4a2eaca98107
Aamir-Yousaf <aamir.yousaf.dev@gmail.com>
2024-06-29 13:58:32 +0500

    add edit functionality

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
   const [todoList, setTodoList] = useState([]);
   const [todo, setTodo] = useState("");
   const [error, setError] = useState(false);
+  const [isEdit, setEdit] = useState(false);
+  const [saveIndex, setIndex] = useState(-1);
 
   const handleSubmit = () => {
     if (todo) {
@@ -33,8 +35,17 @@ function App() {
   };
  const handleDeleted=(index) =>{
   let newTodoLists = [...todoList];
+  if(newTodoLists[index].markAsRead){
   newTodoLists.splice(index,1);
   setTodoList(newTodoLists);
+
+  };
+  
+ }
+ const handleEdit=(index) =>{
+  let newTodoLists = [...todoList];
+  setTodo(newTodoLists[index].text);
+  setEdit(true);
  }
   return (
     <div className="App">
@@ -45,7 +56,8 @@ function App() {
         onChange={handleChange}
         value={todo}
       />
-      <button onClick={handleSubmit}>Submit Todo</button>
+      <button onClick={handleSubmit}>{isEdit ? "Edit Todo" :"Add Todo"}
+      </button>
       {error ? <p>Input is empty</p> : null}
       {todoList.map((item, i) => (
         <div key={i}>
@@ -58,6 +70,7 @@ function App() {
           </p>
           <button onClick={() => handleMarkAsRead(i)}>Mark As Read</button>
           <button onClick={() => handleDeleted(i)}>Delete</button>
+          <button onClick={() => handleEdit(i)}>Edit</button>
         </div>
       ))} 
     </div>
